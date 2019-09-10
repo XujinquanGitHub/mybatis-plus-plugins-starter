1bca22cab2a50e6960603ffa89d88d2e3b9b0490
许金泉 <xujinquan@tuhu.cn>
2019-09-10 09:51:22 +0800

    初始化项目

---
+++ src/main/java/com/github/mybatispluspluginsstarter/MybatisPlusPluginsStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.mybatispluspluginsstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusPluginsStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusPluginsStarterApplication.class, args);
+    }
+
+}
